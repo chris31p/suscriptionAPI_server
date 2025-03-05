32b014e486ca8f817ee00063a5b0510c39738dd0
Christopher <chriuser.email christopher.a31@hotmail.comgit config --global user.name Chris>
2025-03-05 15:02:54 -0500

    Feat: agregar montrolador y ruta para categoria

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -19,12 +19,12 @@ export const addCategoryController = async (req, res) => {
 
     const saveCategory = await addCategory.save();
 
-    if(!saveCategory){
-        return res.status(500).json({
-            msg: "No se pudo crear",
-            error: true,
-            success: false
-        })
+    if (!saveCategory) {
+      return res.status(500).json({
+        msg: "No se pudo crear",
+        error: true,
+        success: false,
+      });
     }
 
     return res.status(201).json({
@@ -42,20 +42,49 @@ export const addCategoryController = async (req, res) => {
   }
 };
 
-export const getCategoryController = async(req, res)=> {
-    try {
-        const data = await CategoryModel.find()
+export const getCategoryController = async (req, res) => {
+  try {
+    const data = await CategoryModel.find();
 
-        return res.status(200).json({
-            data: data,
-            error: false,
-            success: true
-        })
-    } catch (error) {
-        return res.status(500).json({
-            msg: error.message || error,
-            error: true,
-            success: false,
-          });
-    }
-}
+    return res.status(200).json({
+      data: data,
+      error: false,
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+};
+
+export const updateCategoryController = async (req, res) => {
+  try {
+    const { _id, name, image } = req.body;
+
+    const update = await CategoryModel.updateOne(
+      {
+        _id: _id,
+      },
+      {
+        name,
+        image,
+      }
+    );
+
+    return res.json({
+      msg: "Actualizado con éxito",
+      data: update,
+      success: true,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+};
--- src/routes/categoryRoute.js
+++ src/routes/categoryRoute.js
@@ -1,12 +1,12 @@
 import { Router} from 'express';
-import { addCategoryController, getCategoryController } from '../controllers/categoryController.js';
+import { addCategoryController, getCategoryController, updateCategoryController } from '../controllers/categoryController.js';
 import auth from '../middleware/auth.js';
 
 const categoryRouter = Router()
 
 categoryRouter.post('/add-category', auth, addCategoryController);
 categoryRouter.get('/get', getCategoryController)
-
+categoryRouter.put('/update', auth, updateCategoryController)
 
 
 export default categoryRouter;
